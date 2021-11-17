91158d13091387464008e29d3ca75ea5029c7c40
ruoxuan1010 <li.ruoxu@northeastern.edu>
2021-11-17 11:50:17 -0800

    mobile friendly design

--- src/pages/FreeGame.jsx
+++ src/pages/FreeGame.jsx
@@ -23,39 +23,42 @@ export function FreeGame() {
     return (
         <div>
             <Nav />
-            <div className="buttons">
-                <button onClick={() => {
-                    dispatch({
-                        type: 'startGame',
-                    })
-                }} className="button">
-                    Start Game
-                </button>
+            <div className="gameContainer">
+                <div className="buttons">
+                    <button onClick={() => {
+                        dispatch({
+                            type: 'startGame',
+                        })
+                    }} className="button">
+                        Start Game
+                    </button>
 
-                <button onClick={() => {
-                    dispatch({
-                        type: 'RESET',
-                    })
-                }} className="button">
-                    Reset
-                </button>
-            </div>
-
-            <div className="freeGameBoard">
-                <div className="hidden">
-                    <h3>Your Board</h3>
-                    <Board player="AI" />
+                    <button onClick={() => {
+                        dispatch({
+                            type: 'RESET',
+                        })
+                    }} className="button">
+                        Reset
+                    </button>
                 </div>
 
-                <div>
-                    <h3>AI’s Board (Your attacks here!)</h3>
-                    <Board player="human" />
+                <div className="freeGameBoard">
+                    <div className="hidden">
+                        <h3>Your Board</h3>
+                        <Board player="AI" />
+                    </div>
+
+                    <div>
+                        <h3>AI’s Board (Your attacks here!)</h3>
+                        <Board player="human" />
+                    </div>
+                </div>
+
+                <div className="center">
+                    <h1>{winMessage}</h1>
                 </div>
             </div>
 
-            <div className="center">
-                <h1>{winMessage}</h1>
-            </div>
 
         </div>);
 
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -26,39 +26,42 @@ export function Game() {
     return (
         <div>
             <Nav />
-            <div className="buttons">
-                <button onClick={() => {
-                    dispatch({
-                        type: 'startGame',
-                    })
-                }} className="button">
-                    Start Game
-                </button>
+            <div className="gameContainer">
+                <div className="buttons">
+                    <button onClick={() => {
+                        dispatch({
+                            type: 'startGame',
+                        })
+                    }} className="button">
+                        Start Game
+                    </button>
 
-                <button onClick={() => {
-                    dispatch({
-                        type: 'RESET',
-                    })
-                }} className="button">
-                    Reset
-                </button>
-            </div>
-
-            <div className="boards">
-                <div>
-                    <h3>Your Board</h3>
-                    <Board player="AI" />
+                    <button onClick={() => {
+                        dispatch({
+                            type: 'RESET',
+                        })
+                    }} className="button">
+                        Reset
+                    </button>
                 </div>
 
-                <div>
-                    <h3>AI’s Board (Your attacks here!)</h3>
-                    <Board player="human" />
+                <div className="boards">
+                    <div>
+                        <h3>Your Board</h3>
+                        <Board player="AI" />
+                    </div>
+
+                    <div>
+                        <h3>AI’s Board (Your attacks here!)</h3>
+                        <Board player="human" />
+                    </div>
+                </div>
+
+                <div className="center">
+                    <h1>{winMessage}</h1>
                 </div>
             </div>
 
-            <div className="center">
-                <h1>{winMessage}</h1>
-            </div>
 
         </div>);
 
